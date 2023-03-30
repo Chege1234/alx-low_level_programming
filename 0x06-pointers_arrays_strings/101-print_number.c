c27bde3696708fb32a7db50fcfe4fb27a280b683
Chege1234 <lewiskariuki04@gmail.com>
2023-03-30 07:20:44 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,6 +5,7 @@
  * @n: integer params
  * Return: 0
  */
+
 void print_number(int n)
 {
 	unsigned int n1;
@@ -13,12 +14,14 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('_');
+		putchar('-');
 		n1 = -n;
 	}
+
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
 	putchar((n1 % 10) + '0');
 }
+
